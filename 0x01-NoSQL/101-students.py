2f9d6a7e413e6a8f63f9145a76226cb7d0af5f3b
MMarzoo <mmarzouk12378@gmail.com>
2024-08-13 16:36:58 +0300

    Task #14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-'''
- MongoDB Operations with Python using pymongo
-'''
+""" MongoDB Operations with Python using pymongo """
 
 
 def top_students(mongo_collection):
-    ''' returns all students sorted by average score '''
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted by average score """
     top_student = mongo_collection.aggregate([
         {
             "$project": {
                 "name": "$name",
-                "averageScores": {"$avg": "$topics.score"},
+                "averageScore": {"$avg": "$topics.score"}
             }
         },
         {"$sort": {"averageScore": -1}}
