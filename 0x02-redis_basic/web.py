885d2a536ddb06d2de91c2378e7d2ae04cd2bc2b
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 23:03:55 +0300

    advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,7 +24,6 @@ def track_get_page(fn: Callable) -> Callable:
     return wrapper
 
 
-@track_get_page
 def get_page(url: str) -> str:
     ''' Function to get page '''
     response = requests.get(url)
