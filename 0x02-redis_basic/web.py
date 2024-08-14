acc690f52273d102af0e29e9fd8ecab2aa7aaef1
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 22:53:02 +0300

    advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,55 +1,35 @@
 #!/usr/bin/env python3
-"""Module for implementing an expiring web cache and tracker
 """
+Caching request module
+"""
+import redis
 import requests
-import time
 from functools import wraps
-
-CACHE_EXPIRATION_TIME = 10  # seconds
-CACHE = {}
+from typing import Callable
 
 
-def cache(fn):
-    """_summary_
-
-    Args:
-        fn (function): _description_
-
-    Returns:
-        _type_: _description_
-    """
+def track_get_page(fn: Callable) -> Callable:
+    ''' Decorator for get_page '''
     @wraps(fn)
-    def wrapped(*args, **kwargs):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
-        url = args[0]
-        if url in CACHE and CACHE[url]["timestamp"] + CACHE_EXPIRATION_TIME > \
-                time.time():
-            CACHE[url]["count"] += 1
-            return CACHE[url]["content"]
-        else:
-            content = fn(*args, **kwargs)
-            CACHE[url] = {"content": content,
-                          "timestamp": time.time(), "count": 1}
-            return content
-    return wrapped
+    def wrapper(url: str) -> str:
+        ''' wrapper function '''
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
 
 
-@cache
+@track_get_page
 def get_page(url: str) -> str:
-    """_summary_
-
-    Args:
-        url (str): _description_
-
-    Returns:
-        str: _description_
-    """
-    global count
-    # increment count
-    count += 1
+    ''' Function to get page '''
     response = requests.get(url)
-    return response.content.decode('utf-8')
+    return response.text
+
+
+if __name__ == '__main__':
+    print(get_page('http://slowwly.robertomurray.co.uk'))
