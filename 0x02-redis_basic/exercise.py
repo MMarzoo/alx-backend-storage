96b93f089663eeb1ed4a3304737cb460d21f6164
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 19:37:15 +0300

    Task #1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,3 +24,24 @@ class Cache:
         client = self._redis
         client.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+        '''  Gets key's value from redis and converts '''
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return value
+
+    def get_str(self, data: bytes) -> str:
+        ''' Converts bytes to string '''
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        ''' Converts bytes to int '''
+        return int(data)
