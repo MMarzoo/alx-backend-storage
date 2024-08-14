48b134120718247062f2163bc6305bcd75bb6b38
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 22:47:44 +0300

    advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,10 +9,14 @@ from typing import Callable
 
 
 def track_get_page(fn: Callable) -> Callable:
-    ''' Decorator for get_page '''
+    """ Decorator for get_page
+    """
     @wraps(fn)
     def wrapper(url: str) -> str:
-        ''' wrapper function '''
+        """ Wrapper that:
+            - check whether a url's data is cached
+            - tracks how many times get_page is called
+        """
         client = redis.Redis()
         client.incr(f'count:{url}')
         cached_page = client.get(f'{url}')
@@ -26,6 +30,7 @@ def track_get_page(fn: Callable) -> Callable:
 
 @track_get_page
 def get_page(url: str) -> str:
-    ''' Function to get page '''
+    """ Makes a http request to a given endpoint
+    """
     response = requests.get(url)
     return response.text
