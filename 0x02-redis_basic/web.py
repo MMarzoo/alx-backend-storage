802f006aea82c7214caf5aa688ac9f8b8de72f41
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 23:03:20 +0300

    advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,72 +1,35 @@
 #!/usr/bin/env python3
-
 """
-Function uses the requests module to obtain the HTML content of a particular
-URL and returns it
+Caching request module
 """
-
-from functools import wraps
-
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
-store = redis.Redis()
 
-
-def count_url_usage(method):
-    """
-    Decorator function to count URL usage and cache the data
-
-    Function caches the result of the method for a given URL
-
-    Parameters
-        url: URL for which the result should be cached
-
-    Return
-        str: Cached HTML content for the given URL
-    """
-
-    @wraps(method)
-    def wrapper(url):
-        """
-        Wrapper function to cache the result of the method for a given URL
-
-        Parameters
-            url: URL for which the result should be cached
-
-        Return
-            str: Cached HTML content for the given URL
-        """
-
-        cached_key = f"cached:{url}"
-        cached_data = store.get(cached_key)
-
-        if cached_data:
-            return cached_data.decode("utf-8")  # type: ignore
-
-        count_key = f"count:{url}"
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+def track_get_page(fn: Callable) -> Callable:
+    ''' Decorator for get_page '''
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        ''' wrapper function '''
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
     return wrapper
 
 
-@count_url_usage
+@track_get_page
 def get_page(url: str) -> str:
-    """
-    Simply counts the number of times the function was accessed
-
-    Retrieves the content of the given URL and returns it as a string
-
-    Parameters
-        url: The URL to retrieve the content from
-
-    Return
-        str: The content of the URL
-    """
-
+    ''' Function to get page '''
     response = requests.get(url)
     return response.text
+
+
+if __name__ == '__main__':
+    print(get_page('http://slowwly.robertomurray.co.uk'))
