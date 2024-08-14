09e1601e33be5eb7faaf132c22ae58869b6e9233
MMarzoo <mmarzouk12378@gmail.com>
2024-08-14 23:10:45 +0300

    advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -34,5 +34,5 @@ def get_page(url: str) -> str:
 if __name__ == '__main__':
     url = 'http://slowwly.robertomurray.co.uk'
 
-print(get_page(url))
-print(get_page(url))
+    print(get_page(url))
+    print(get_page(url))
